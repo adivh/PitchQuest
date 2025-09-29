b141ace8410635ee43dbbb43fe216f0c4f82624c
adivh <adrian.vonheyl@gmail.com>
2025-09-29 14:27:18 +0200

    Style changes

--- include/pitchquest/scale.hpp
+++ include/pitchquest/scale.hpp
@@ -2,16 +2,14 @@
 
 #include <cmath>
 
-namespace pitchquest {
-    class Scale {
-        public:
-            Scale() = delete;
-            Scale(const Scale& other) = delete;
-            Scale(const Scale&& other) = delete;
-            ~Scale() = delete;
+namespace PitchQuest {
 
-            static constexpr double Heptatonic(int note, int octave) noexcept {
-                return 440.0 * std::pow(2.0, ((note - 9) + 12 * (octave - 4)) / 12.0);        // A4 <= Heptatonic(9, 4)
-            }
-    };
+namespace Scale {
+
+constexpr double heptatonic(int note, int octave) noexcept {
+    return 440.0 * std::pow(2.0, ((note - 9) + 12 * (octave - 4)) / 12.0);        // A4 <= Heptatonic(9, 4)
 }
+
+}   // namespace Scale
+
+}   // namespace PitchQuest
--- test/test_scale.cpp
+++ test/test_scale.cpp
@@ -2,46 +2,46 @@
 
 #include "scale.hpp"
 
-using namespace pitchquest;
+using namespace PitchQuest;
 
-const double EPSILON {1e-2};
+const double epsilon {1.0e-2};
 
-TEST(ScaleTest, HeptatonicC0) {
-    EXPECT_NEAR(Scale::Heptatonic(0, 0), 16.35, EPSILON);
+TEST(ScaleTest, heptatonicC0) {
+    EXPECT_NEAR(Scale::heptatonic(0, 0), 16.35, epsilon);
 }
 
-TEST(ScaleTest, HeptatonicC8) {
-    EXPECT_NEAR(Scale::Heptatonic(0, 8), 4186.0, EPSILON);
+TEST(ScaleTest, heptatonicC8) {
+    EXPECT_NEAR(Scale::heptatonic(0, 8), 4186.0, epsilon);
 }
 
-TEST(ScaleTest, HeptatonicFs3) {
-    EXPECT_NEAR(Scale::Heptatonic(6, 3), 185.0, EPSILON);
+TEST(ScaleTest, heptatonicFs3) {
+    EXPECT_NEAR(Scale::heptatonic(6, 3), 185.0, epsilon);
 }
 
-TEST(ScaleTest, HeptatonicA0) {
-    EXPECT_NEAR(Scale::Heptatonic(9, 0), 27.5, EPSILON);
+TEST(ScaleTest, heptatonicA0) {
+    EXPECT_NEAR(Scale::heptatonic(9, 0), 27.5, epsilon);
 }
 
-TEST(ScaleTest, HeptatonicA4) {
-    EXPECT_NEAR(Scale::Heptatonic(9, 4), 440.0, EPSILON);
+TEST(ScaleTest, heptatonicA4) {
+    EXPECT_NEAR(Scale::heptatonic(9, 4), 440.0, epsilon);
 }
 
-TEST(ScaleTest, HeptatonicA5) {
-    EXPECT_NEAR(Scale::Heptatonic(9, 5), 880.0, EPSILON);
+TEST(ScaleTest, heptatonicA5) {
+    EXPECT_NEAR(Scale::heptatonic(9, 5), 880.0, epsilon);
 }
 
-TEST(ScaleTest, HeptatonicAs4) {
-    EXPECT_NEAR(Scale::Heptatonic(10, 4), 466.16, EPSILON);
+TEST(ScaleTest, heptatonicAs4) {
+    EXPECT_NEAR(Scale::heptatonic(10, 4), 466.16, epsilon);
 }
 
-TEST(ScaleTest, HeptatonicB0) {
-    EXPECT_NEAR(Scale::Heptatonic(11, 0), 30.87, EPSILON);
+TEST(ScaleTest, heptatonicB0) {
+    EXPECT_NEAR(Scale::heptatonic(11, 0), 30.87, epsilon);
 }
 
-TEST(ScaleTest,HeptatonicB7) {
-    EXPECT_NEAR(Scale::Heptatonic(11, 7), 3951.07, EPSILON);
+TEST(ScaleTest,heptatonicB7) {
+    EXPECT_NEAR(Scale::heptatonic(11, 7), 3951.07, epsilon);
 }
 
-TEST(ScaleTest, HeptatonicB8) {
-    EXPECT_NEAR(Scale::Heptatonic(11, 8), 7902.13, EPSILON);
+TEST(ScaleTest, heptatonicB8) {
+    EXPECT_NEAR(Scale::heptatonic(11, 8), 7902.13, epsilon);
 }
