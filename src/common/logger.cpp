7374902e15baaf25fa35213ffd5af97472a7f6e1
adivh <adrian.vonheyl@gmail.com>
2025-10-19 19:55:03 +0200

    Moved setup_logger definition to source file
    This avoids multiple definitions in the future

---
+++ src/common/logger.cpp
@@ -0,0 +1,17 @@
+#include "logger.hpp"
+
+namespace PitchQuest {
+
+void setup_logger() {
+        auto console_sink {std::make_shared<spdlog::sinks::stdout_color_sink_mt>()};
+        console_sink->set_level(spdlog::level::debug);
+
+        auto file_sink {std::make_shared<spdlog::sinks::basic_file_sink_mt>("logs/log.txt")};
+        file_sink->set_level(spdlog::level::warn);
+
+        auto logger {std::make_shared<spdlog::logger>("multi_sink", spdlog::sinks_init_list{console_sink, file_sink})};
+        logger->set_level(spdlog::level::trace);
+        spdlog::set_default_logger(logger);
+}
+
+}   // namespace PitchQuest
