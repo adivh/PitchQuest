edc2885d4f3afb21557a2e900b90ab596ac45f8a
adivh <adrian.vonheyl@gmail.com>
2025-10-19 21:49:01 +0200

    Automatically create logs folder and log more messages in the file sink

--- src/common/logger.cpp
+++ src/common/logger.cpp
@@ -1,17 +1,21 @@
 #include "logger.hpp"
 
+#include <filesystem>
+
 namespace PitchQuest {
 
 void setup_logger() {
-        auto console_sink {std::make_shared<spdlog::sinks::stdout_color_sink_mt>()};
-        console_sink->set_level(spdlog::level::debug);
+    std::filesystem::create_directories("logs");
 
-        auto file_sink {std::make_shared<spdlog::sinks::basic_file_sink_mt>("logs/log.txt")};
-        file_sink->set_level(spdlog::level::warn);
+    auto console_sink {std::make_shared<spdlog::sinks::stdout_color_sink_mt>()};
+    console_sink->set_level(spdlog::level::debug);
 
-        auto logger {std::make_shared<spdlog::logger>("multi_sink", spdlog::sinks_init_list{console_sink, file_sink})};
-        logger->set_level(spdlog::level::trace);
-        spdlog::set_default_logger(logger);
+    auto file_sink {std::make_shared<spdlog::sinks::basic_file_sink_mt>("logs/log.txt")};
+    file_sink->set_level(spdlog::level::info);
+
+    auto logger {std::make_shared<spdlog::logger>("multi_sink", spdlog::sinks_init_list{console_sink, file_sink})};
+    logger->set_level(spdlog::level::trace);
+    spdlog::set_default_logger(logger);
 }
 
 }   // namespace PitchQuest
