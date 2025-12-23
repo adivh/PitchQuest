939983737297eadb0fd5e3bfdf7d56b1b3bddd4c
adivh <adrian.vonheyl@gmail.com>
2025-12-23 10:34:31 +0100

    Changed all IntervalChallengePacket CTOR to constexpr

--- include/pitchquest/intervalchallengepacket.hpp
+++ include/pitchquest/intervalchallengepacket.hpp
@@ -7,8 +7,19 @@
 namespace PitchQuest {
 
 struct IntervalChallengePacket {
-    IntervalChallengePacket(uint8_t sound1, uint8_t sound2);
-    IntervalChallengePacket(uint8_t note1, uint8_t octave1, uint8_t note2, uint8_t octave2);
+    constexpr IntervalChallengePacket(uint8_t sound1, uint8_t sound2)
+        : IntervalChallengePacket {
+            static_cast<uint8_t>(sound1 >> 4),
+            static_cast<uint8_t>(sound1 & 0x0f),
+            static_cast<uint8_t>(sound2 >> 4),
+            static_cast<uint8_t>(sound2 & 0x0f)
+        } {};
+
+    constexpr IntervalChallengePacket(uint8_t note1, uint8_t octave1, uint8_t note2, uint8_t octave2)
+        :   note1{note1},
+            octave1{octave1},
+            note2{note2},
+            octave2{octave2} {};
 
     static inline const uint8_t packet_type {0xb1};
     const uint8_t note1;
