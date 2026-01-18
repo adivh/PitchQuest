0f6af7696f1716d2afa72d0b26c2ca3356652a46
adivh <adrian.vonheyl@gmail.com>
2026-01-18 23:50:08 +0100

    Added tokenize function

--- include/pitchquest/cli.hpp
+++ include/pitchquest/cli.hpp
@@ -7,7 +7,13 @@
 
 namespace PitchQuest::cli {
 
-    inline const char delimiter = ' ';
+    /**
+     *  Value is used to improve performance by avoiding allocations.
+     *  Changing to any positive value or zero is safe.
+     */
+    inline const int heuristic_command_token_count {3};
+
+    inline const char delimiter {' '};
 
     inline void sanitize(std::string& command) {
         std::transform(command.begin(), command.end(), command.begin(), [](unsigned char c){ return std::tolower(c); });
@@ -17,6 +23,30 @@ namespace PitchQuest::cli {
         std::string s {command};
         sanitize(s);
         return s;
+    }
+
+    inline std::vector<std::string> tokenize(std::string_view command) {
+        std::vector<std::string> tokens {};
+        tokens.reserve(heuristic_command_token_count);
+
+        size_t index_start {};
+        size_t index_end {};
+
+        while (index_start < command.size()) {
+            index_end = command.find(delimiter, index_start);
+
+            if (index_end == std::string::npos) {
+                tokens.emplace_back(command.substr(index_start, command.size() - index_start));
+                return tokens;
+            } else if (index_end > index_start) {
+                tokens.emplace_back(command.substr(index_start, index_end - index_start));
+                index_start += index_end - index_start;
+            } else {
+                index_start++;
+            }
+        }
+
+        return tokens;
     }
     
 }   // namespace PitchQuest::cli
---
+++ test/test_tokenize.cpp
@@ -0,0 +1,60 @@
+#include <gtest/gtest.h>
+
+#include "cli.hpp"
+
+TEST(TokenizeTest, TokenizeEmpty) {
+    std::vector<std::string> tokens {PitchQuest::cli::tokenize("")};
+    EXPECT_EQ(tokens.size(), 0);
+}
+
+TEST(TokenizeTest, TokenizeOneSpace) {
+    std::vector<std::string> tokens {PitchQuest::cli::tokenize(" ")};
+    EXPECT_EQ(tokens.size(), 0);
+}
+
+TEST(TokenizeTest, TokenizeThreeSpaces) {
+    std::vector<std::string> tokens {PitchQuest::cli::tokenize("   ")};
+    EXPECT_EQ(tokens.size(), 0);
+}
+
+TEST(TokenizeTest, TokenizeOne) {
+    std::vector<std::string> tokens_quit {PitchQuest::cli::tokenize("quit")};
+    EXPECT_EQ(tokens_quit.size(), 1);
+    EXPECT_EQ(tokens_quit[0], "quit");
+    
+    std::vector<std::string> tokens_exit {PitchQuest::cli::tokenize("exit")};
+    EXPECT_EQ(tokens_exit.size(), 1);
+    EXPECT_EQ(tokens_exit[0], "exit");
+}
+
+TEST(TokenizeTest, TokenizeOneLeadingSpace) {
+    std::vector<std::string> tokens {PitchQuest::cli::tokenize(" leading")};
+    EXPECT_EQ(tokens.size(), 1);
+    EXPECT_EQ(tokens[0], "leading");
+}
+
+TEST(TokenizeTest, TokenizeOneTrailingSpace) {
+    std::vector<std::string> tokens {PitchQuest::cli::tokenize("trailing ")};
+    EXPECT_EQ(tokens.size(), 1);
+    EXPECT_EQ(tokens[0], "trailing");
+}
+
+TEST(TokenizeTest, TokenizeSentence) {
+    std::vector<std::string> tokens {PitchQuest::cli::tokenize("A good listener is a silent flatterer")};
+    EXPECT_EQ(tokens.size(), 7);
+    EXPECT_EQ(tokens[0], "A");
+    EXPECT_EQ(tokens[1], "good");
+    EXPECT_EQ(tokens[2], "listener");
+    EXPECT_EQ(tokens[3], "is");
+    EXPECT_EQ(tokens[4], "a");
+    EXPECT_EQ(tokens[5], "silent");
+    EXPECT_EQ(tokens[6], "flatterer");
+}
+
+TEST(TokenizeTest, TokenizeSentenceExcessSpaces) {
+    std::vector<std::string> tokens {PitchQuest::cli::tokenize("   All that   glitters  ")};
+    EXPECT_EQ(tokens.size(), 3);
+    EXPECT_EQ(tokens[0], "All");
+    EXPECT_EQ(tokens[1], "that");
+    EXPECT_EQ(tokens[2], "glitters");
+}
